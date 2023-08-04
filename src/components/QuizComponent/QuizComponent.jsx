bc3493fb61f597fc44c64bdc6f1236dff9325da9
Buttarelli Lucas <buttarelli.lucas@gmail.com>
2023-08-04 15:49:03 -0300

    ultimas modificaciones y mejoras de filtro en el codigo

--- src/components/QuizComponent/QuizComponent.jsx
+++ src/components/QuizComponent/QuizComponent.jsx
@@ -1,46 +1,52 @@
-// Importa las dependencias necesarias
+// Importamos las dependencias necesarias
 import React, { useState, useEffect } from 'react';
-import unorm from 'unorm'; // Importa la función nfc de unorm para normalizar cadenas de texto con tildes
-import Database from '../../../data'; // Importa la base de datos original (quiz de preguntas)
-
-// Importa los estilos del componente
+import Database from '../../../data';
 import './QuizComponent.css';
 
+// Función para eliminar acentos de una cadena de texto
+const removeAccents = (str) => {
+  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+};
+
+// Definimos el componente funcional QuizComponent
 const QuizComponent = () => {
-  // Estados para el término de búsqueda, carga, saludo y saludo final
+  // Definimos los estados del componente usando el Hook useState
   const [searchTerm, setSearchTerm] = useState('');
   const [isLoading, setIsLoading] = useState(true);
   const [greeting, setGreeting] = useState('');
   const [finalSaludo, setFinalSaludo] = useState('');
 
-  // Función para manejar cambios en el término de búsqueda
+  // Manejador de cambios en el campo de búsqueda
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  // Función para manejar cambios en el saludo final
+  // Manejador de cambios en el campo de saludo final
   const handleFinalSaludo = (event) => {
     setFinalSaludo(event.target.value);
   };
 
-  // Función para obtener las preguntas que coinciden con el término de búsqueda
+  // Función para obtener las preguntas coincidentes con el término de búsqueda
   const getMatchingQuestions = () => {
-    const normalizedSearchTerm = unorm.nfc(searchTerm.toLowerCase().trim()); // Normaliza el término de búsqueda
+    // Eliminamos acentos y convertimos el término de búsqueda a minúsculas
+    const normalizedSearchTerm = removeAccents(searchTerm.toLowerCase().trim());
+    // Dividimos el término en palabras clave
     const searchKeywords = normalizedSearchTerm.split(/\s+/);
 
+    // Filtramos la base de datos para obtener las preguntas coincidentes
     return Database.filter((question) => {
       return searchKeywords.every((keyword) =>
-        unorm.nfc(question.articulo.toLowerCase()).includes(keyword) ||
-        unorm.nfc(question.pregunta.toLowerCase()).includes(keyword) ||
-        unorm.nfc(question.sku.toLowerCase()).includes(keyword)
+        removeAccents(question.articulo.toLowerCase()).includes(keyword) ||
+        removeAccents(question.pregunta.toLowerCase()).includes(keyword) ||
+        removeAccents(question.sku.toLowerCase()).includes(keyword)
       );
     });
   };
 
-  // Obtiene las preguntas que coinciden con el término de búsqueda
+  // Obtenemos las preguntas coincidentes usando la función getMatchingQuestions
   const matchingQuestions = getMatchingQuestions();
 
-  // Efecto para establecer el saludo adecuado según la hora del día
+  // Efecto para definir el saludo según la hora del día
   useEffect(() => {
     const currentHour = new Date().getHours();
     let greetingText = '';
@@ -54,7 +60,7 @@ const QuizComponent = () => {
     setGreeting(greetingText);
   }, [searchTerm]);
 
-  // Efecto para simular una carga y luego mostrar el contenido
+  // Efecto para simular una carga con un timer
   useEffect(() => {
     setIsLoading(true);
 
@@ -87,13 +93,14 @@ const QuizComponent = () => {
     }));
   };
 
-  // Agrupa las preguntas y respuestas por artículo
+  // Agrupamos las preguntas coincidentes por artículo usando la función groupQuestionsByArticle
   const groupedQuestions = groupQuestionsByArticle(matchingQuestions);
 
+  // Finalmente, renderizamos el contenido del componente
   return (
     <div>
-      {/* Barra de búsqueda */}
       <div>
+        {/* Campo de búsqueda */}
         <p>Ingrese SKU, palabra clave de la pregunta, o parte del nombre de la publicación</p>
         <input
           type="text"
@@ -103,9 +110,9 @@ const QuizComponent = () => {
         />
       </div>
       <br />
-      {/* Input para el saludo final */}
       <div>
-        <p>Ingrese saludo final, (PC Center/2012PC/Tienda Oficial) o su nombre, Tambien puede quedar el espacio vacio</p>
+        {/* Campo de saludo final */}
+        <p>Ingrese saludo final, (PC Center/2012PC/Tienda Oficial) o su nombre, También puede quedar el espacio vacío</p>
         <input
           type="text"
           placeholder="Escribe tu saludo final aquí..."
@@ -115,36 +122,43 @@ const QuizComponent = () => {
       </div>
       <br />
       <br />
-      {/* Muestra un mensaje de carga o las preguntas y respuestas */}
+      {/* Condicional ternario para renderizar el contenido */}
       {isLoading ? (
-        <p>Cargando...</p>
+        // Muestra el mensaje de carga si isLoading es true
+        <p className="articulo-title">Cargando...</p>
       ) : matchingQuestions.length > 0 ? (
-        // Contenedor para mostrar las preguntas y respuestas agrupadas por artículo
+        // Si hay preguntas coincidentes, renderiza el contenedor de tarjetas
         <div className="cards-container">
+          {/* Mapeamos las preguntas coincidentes y generamos las tarjetas */}
           {groupedQuestions.map((group, index) => (
             <div key={group.articulo} className="card">
+              {/* Mostramos el título del artículo */}
               <h2 className="articulo-title">Artículo:</h2>
               <p className="articulo-name">{group.articulo}</p>
 
+              {/* Mostramos el título Preguntas y Respuestas */}
               <h2>Preguntas y Respuestas:</h2>
-              {/* Mapea las preguntas y respuestas del artículo actual */}
+              {/* Mapeamos las preguntas y respuestas del grupo */}
               {group.preguntas.map((pregunta, i) => (
                 <div key={i}>
+                  {/* Mostramos cada pregunta */}
                   <p className="pregunta-text">
                     <strong>{i + 1}. {pregunta} </strong>
                   </p>
-                  {/* Muestra el saludo, la respuesta y el saludo final */}
-                  <p className="respuesta-text"> {greeting}. {group.respuestas[i]} Saludos. {finalSaludo} </p>
+                  {/* Mostramos la respuesta, incluyendo el saludo inicial y el saludo final */}
+                  <p className="respuesta-text">{greeting}. {group.respuestas[i]} Saludos, {finalSaludo}</p>
                 </div>
               ))}
             </div>
           ))}
         </div>
       ) : (
+        // Si no hay preguntas coincidentes, muestra un mensaje de que no se encontraron resultados
         <p>No se encontraron preguntas coincidentes.</p>
       )}
     </div>
   );
 };
 
+// Exportamos el componente QuizComponent
 export default QuizComponent;
